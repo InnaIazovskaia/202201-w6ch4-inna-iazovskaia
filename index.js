c8d7987cd72e6a3877b2a3dd45372a420821a5a8
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-17 20:13:44 +0100

    imported connectDataBase in index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const debug = require("debug")("things:root");
 const initializeServer = require("./server");
+const connectDataBase = require("./db");
 
 const port = process.env.SERVER_PORT || 4000;
+const mongoConnect = process.env.MONGODB_STRING;
 
 (async () => {
   try {
     await initializeServer(port);
-    debug("Server listening");
+    await connectDataBase(mongoConnect);
   } catch (error) {
     debug(error.message);
   }
