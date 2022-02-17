1d96df2a706ed6fe712a9bc58299f377b47cb52f
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-17 22:24:38 +0100

    created Thing.js

---
+++ db/models/Thing.js
@@ -0,0 +1,15 @@
+const { model, Schema } = require("mongoose");
+
+const ThingSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+});
+
+const Thing = model("Thing", ThingSchema, "Things");
+
+module.exports = Thing;
